4aa0803d6c146bbfe0eaa15128655ee610aa6f5a
Devin Smith Jr <devin@devinoz.com>
2020-02-03 14:19:03 -0600

    Add VPC and subnets

--- config.tf
+++ config.tf
@@ -3,6 +3,55 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create VPC
+resource "aws_vpc" "test-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create Sub1
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "${aws_vpc.test-vpc.id}"
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Sub1"
+  }
+}
+
+# Create Sub2
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "${aws_vpc.test-vpc.id}"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Sub2"
+  }
+}
+
+# Create Sub3
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "${aws_vpc.test-vpc.id}"
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "Sub3"
+  }
+}
+
+# Create Sub4
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "${aws_vpc.test-vpc.id}"
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1d"
+
+  tags = {
+    Name = "Sub4"
+  }
+}
+
 # Get the latest RHEL 8 image ID
 data "aws_ami" "redhat" {
   most_recent = true
