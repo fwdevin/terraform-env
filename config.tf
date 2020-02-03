712b30e2950e0b2fecf5c8a140399ac137713ff2
Devin Smith Jr <devin@devinoz.com>
2020-02-03 14:06:02 -0600

    Add EBS volume

--- config.tf
+++ config.tf
@@ -17,7 +17,12 @@ data "aws_ami" "redhat" {
 
 # Create a t2.micro EC2 instance type 
 # (Standard_DS1_v2 seems to be an Azure type)
-resource "aws_instance" "test" {
+resource "aws_instance" "test-instance" {
   ami           = "${data.aws_ami.redhat.id}"
   instance_type = "t2.micro"
+}
+
+resource "aws_ebs_volume" "test-volume" {
+  availability_zone = "us-east-1a"
+  size              = 20
 }
