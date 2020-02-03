2486b50cc018bf4ed5e3580b72a76695a4e5de8e
Devin Smith Jr <devin@devinoz.com>
2020-02-03 13:52:50 -0600

    Add Terraform config file

---
+++ config.tf
@@ -0,0 +1,28 @@
+# Add AWS as a provider and configure the default region
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Get the latest RHEL 8 image ID
+data "aws_ami" "redhat" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["RHEL-8.0.0_HVM"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["309956199498"] # Red Hat
+}
+
+# Create a t2.micro EC2 instance type 
+# (Standard_DS1_v2 seems to be an Azure type)
+resource "aws_instance" "test" {
+  ami           = "${data.aws_ami.redhat.id}"
+  instance_type = "t2.micro"
+}
